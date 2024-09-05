8d48ecf7982f7e9418fcab907682cf9d07c7c0e6
Carla <carlarhentschel@gmail.com>
2024-09-05 12:06:14 -0300

    feat: create banner component

---
+++ src/components/MainBanner.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export function MainBanner() {
+    return (
+        <div className="relative w-full flex items-center justify-center">
+            <Image 
+                src="/assets/SnapInsta_277051709_862524961814466_3973340754104546427_n_AdobeCreativeCloudExpress 1.png" 
+                alt="Banner"
+                layout="responsive"
+                width={1440}
+                height={720}
+                priority
+                className="object-cover"
+            />
+            <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+            <button 
+                className="bg-transparent hover:bg-opacity-20 text-white py-3 px-5 rounded-[32px]  border border-white"
+                style={{ width: '106px', height: '56px' }}
+            >
+                    CONFIRA
+                </button>
+            </div>
+        </div>
+    )
+}
