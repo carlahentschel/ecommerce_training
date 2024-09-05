84d3ee820f08b634fb1b8e34589cef05f23d9d2d
Carla <carlarhentschel@gmail.com>
2024-09-05 12:07:04 -0300

    feat: add banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { MainBanner } from "@/components/MainBanner";
 import { Header } from "../components/Header";
 
 export default function Home() {
   return (
     <main>
        <Header/>
+       <MainBanner />
     </main>
   );
 }
