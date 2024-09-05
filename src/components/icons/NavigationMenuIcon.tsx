b87d6ea1a668d98548da27f5580b434fe0ddce66
Carla <carlarhentschel@gmail.com>
2024-09-05 14:53:08 -0300

    feat: create navigation menu icon

---
+++ src/components/icons/NavigationMenuIcon.tsx
@@ -0,0 +1,9 @@
+export function NavigationMenuIcon() {
+    return (
+        <button className="p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+        </button>
+    )
+}
