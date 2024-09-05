f95426bb23ac8752342db29678d767d4f98250e1
Carla <carlarhentschel@gmail.com>
2024-09-05 16:00:59 -0300

    feat: creates promotional banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { MainBanner } from "@/components/MainBanner";
 import { Header } from "../components/Header";
+import { PromotionalBanner } from "@/components/PromotionalBanner";
 
 export default function Home() {
   return (
     <main>
        <Header/>
        <MainBanner />
+       <PromotionalBanner/>
     </main>
   );
 }
