b037f11a55af15fb4737b8e6ecf2966260bd2bd2
Carla <carlarhentschel@gmail.com>
2024-09-09 17:33:02 -0300

    style: adjust background color

--- src/components/ProductItemCard.tsx
+++ src/components/ProductItemCard.tsx
@@ -1,7 +1,7 @@
 export function ProductItemCard() {
     return (
         <section className="relative w-full w-[286px] h-[440px] mr-2">
-            <div className="w-full h-[392px]  bg-slate-100">
+            <div className="w-full h-[392px] bg-[#F2F2F2]">
                 {/* aqui deve inserir a imagem do produto */}
             </div>
             <div className="flex items-center justify-center h-[48px] w-[254px] mx-auto">
