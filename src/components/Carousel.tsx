52b5b6029201f9186e2111fa08f90a23081dad43
Carla <carlarhentschel@gmail.com>
2024-09-09 17:15:45 -0300

    style: adjust style

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,13 +3,13 @@ import { ProductItemCard } from "./ProductItemCard";
 
 export function Carousel() {
     return (
-        <section className="relative w-full h-[649px] flex items-center justify-center bg-slate-200">
+        <section className="relative w-full h-[649px] flex items-center justify-center">
             <div className="absolute w-full h-[553px] flex flex-col justify-center">
                 <div className="flex justify-between items-center w-full h-8 px-6 ">
                     <h3 className="text-2xl font-light leading-8 tracking-[0.2em] text-left">
                         APOSTAS DA SEMANA
                     </h3>
-                    <Link href="/todos-os-produtos" className="text-xs font-normal leading-4 text-right">
+                    <Link href="/todos-os-produtos" className="text-xs font-normal leading-4 text-right underline">
                         VER TODOS OS PRODUTOS
                     </Link>
                 </div>
