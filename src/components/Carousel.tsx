3d5c778726de076a59e80331f8dae6e573c739e0
Carla <carlarhentschel@gmail.com>
2024-09-10 15:56:05 -0300

    feat: search for products in the api

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,24 @@
 import Link from "next/link";
-import { ProductItemCard } from "./ProductItemCard";
+import { IProduct, ProductItemCard } from "./ProductItemCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Carousel() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    const fetchProducts = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/products');
+            setProducts(response.data);
+        } catch (err) {
+            console.error('Erro ao carregar os produtos');
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     return (
         <section className="relative w-full h-[649px] flex items-center justify-center">
             <div className="absolute w-full h-[553px] flex flex-col justify-center">
@@ -14,11 +31,7 @@ export function Carousel() {
                     </Link>
                 </div>
                 <div className="flex-grow flex items-center justify-start">
-                    <ProductItemCard />
-                    <ProductItemCard />
-                    <ProductItemCard />
-                    <ProductItemCard />
-                    <ProductItemCard />
+                    <ProductItemCard products={products} />
                 </div>
             </div>
         </section>
